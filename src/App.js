aa3a3a0a00a0eb2beb6b9a0652b14fd980815c22
Arter Tendean <arter.tendean.07@gmail.com>
2021-04-13 16:44:53 +0800

    Adding Login Page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import Navbar from "./components/navbar";
+import Login from "./components/login";
 
 function App() {
-  return <Navbar />;
+  return (
+    <div>
+      <Navbar />
+
+      <div className="container-fluid p-2">
+        <Login />
+      </div>
+    </div>
+  );
 }
 
 export default App;
