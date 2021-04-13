1934f09f204c8ca4635940e57914a502ca8c40ec
Arter Tendean <arter.tendean.07@gmail.com>
2021-04-13 16:58:44 +0800

    Adding Register Page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Navbar from "./components/navbar";
 import Login from "./components/login";
+import Register from "./components/register";
 
 function App() {
   return (
     <div>
       <Navbar />
 
-      <div className="container-fluid p-2">
-        <Login />
+      <div className="container-fluid mt-3 p-2">
+        {/* <Login /> */}
+        <Register />
       </div>
     </div>
   );
